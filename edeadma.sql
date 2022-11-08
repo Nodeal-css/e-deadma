adac7d2b31d1e8a31fa8fe087821970b22a26978
Dael Mondigo <56195546+Nodeal-css@users.noreply.github.com>
2022-11-08 22:13:40 +0800

    11/08/2022 updating edeadma.sql

--- edeadma.sql
+++ edeadma.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 29, 2022 at 01:24 PM
+-- Generation Time: Nov 08, 2022 at 02:12 PM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -93,10 +93,23 @@ DROP TABLE IF EXISTS `cemetery_deed`;
 CREATE TABLE IF NOT EXISTS `cemetery_deed` (
   `deed_id` int(11) NOT NULL AUTO_INCREMENT,
   `plot_id` int(11) NOT NULL,
-  `document` blob,
+  `document` varchar(120) DEFAULT NULL,
   PRIMARY KEY (`deed_id`),
-  KEY `plot_id` (`plot_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  UNIQUE KEY `plot_id` (`plot_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cemetery_deed`
+--
+
+INSERT INTO `cemetery_deed` (`deed_id`, `plot_id`, `document`) VALUES
+(60, 27, 'D:/WAMP/www/edeadma/documents/1667916521Assignment 2.pdf'),
+(61, 28, 'D:/WAMP/www/edeadma/documents/1667913984Assignment 1.pdf'),
+(62, 29, 'D:/WAMP/www/edeadma/documents/1667914003Story Analysis.pdf'),
+(63, 36, 'D:/WAMP/www/edeadma/documents/1667914113Finals.pdf'),
+(64, 30, 'D:/WAMP/www/edeadma/documents/1667914193Story Analysis.pdf'),
+(65, 31, 'D:/WAMP/www/edeadma/documents/1667914216Data Dictionary.pdf'),
+(66, 34, 'D:/WAMP/www/edeadma/documents/1667916496ERDFinal.pdf');
 
 -- --------------------------------------------------------
 
@@ -128,9 +141,9 @@ CREATE TABLE IF NOT EXISTS `deceased` (
 
 INSERT INTO `deceased` (`deceased_id`, `grave_id`, `cemetery_id`, `fname`, `lname`, `mi`, `burial_date`, `birth_date`, `marital_status`, `age`, `epitaph`) VALUES
 (1, NULL, 2, 'Hamon', 'user', 'R', '2022-10-07', '2022-10-28', 'Divorced', 66, 'Rest in Peace'),
-(2, NULL, 3, 'Jacky', 'Luv', 'A', '2022-02-04', '2022-11-30', 'Single', 21, 'To God be all the glory'),
-(3, NULL, 3, 'Tom', 'Innit', 'Q', '2022-01-01', '2022-12-31', 'Married', 67, 'In loving memory'),
-(4, NULL, 3, 'Galg', 'Jeager', 'F', '2022-10-01', '2022-10-29', 'Separated', 19, 'Holy'),
+(2, 1, 3, 'Jacky', 'Luv', 'A', '2022-02-04', '2022-11-30', 'Single', 21, 'To God be all the glory'),
+(3, 2, 3, 'Tom', 'Innit', 'Q', '2022-01-01', '2022-12-31', 'Married', 67, 'In loving memory'),
+(4, 2, 3, 'Galg', 'Jeager', 'F', '2022-10-01', '2022-10-29', 'Separated', 19, 'Holy'),
 (5, NULL, 3, 'Hosha', 'Shin', 'T', '2022-10-14', '2022-10-07', 'Widowed', 44, 'Holy'),
 (6, NULL, 3, 'Ann', 'Greek', 'D', '2022-10-06', '2022-10-28', 'Married', 11, 'Rest in Peace');
 
@@ -154,7 +167,15 @@ CREATE TABLE IF NOT EXISTS `grave_location` (
   `y4` varchar(15) NOT NULL,
   PRIMARY KEY (`grave_id`),
   KEY `cemetery_id` (`cemetery_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `grave_location`
+--
+
+INSERT INTO `grave_location` (`grave_id`, `cemetery_id`, `x1`, `y1`, `x2`, `y2`, `x3`, `y3`, `x4`, `y4`) VALUES
+(1, 3, '904', '527', '925', '598', '974', '585', '544', '495'),
+(2, 3, '904', '527', '925', '598', '1004', '585', '106', '989');
 
 -- --------------------------------------------------------
 
@@ -174,15 +195,20 @@ CREATE TABLE IF NOT EXISTS `owner` (
   `phone_no` varchar(15) DEFAULT NULL,
   `email` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`owner_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `owner`
 --
 
 INSERT INTO `owner` (`owner_id`, `fname`, `lname`, `mi`, `street`, `city`, `zip`, `phone_no`, `email`) VALUES
-(1, 'Wilma', 'Sooth', 'Y', 'St. Ronald street', 'Davao City', '7712', '0918242152', 'example@example.com'),
-(2, 'Anthony', 'Parker', 'F', 'St. Martha street', 'Naga City', '2451', '09884512231', 'example@example.com');
+(12, 'Recardo', 'Ronaldo', 'F', 'San Roque St.', 'Argao City', '2131', '988716211', 'recardo@example.com'),
+(13, 'Fejard', 'Fajardo', 'M', 'St. August st.', 'Sibonga', '1872', '922121131', 'Fejard@example.com'),
+(14, 'Hanny', 'Enriquez', 'R', 'San Vicente St.', 'Carcar City', '9211', '98872124', 'Hanny@example.com'),
+(15, 'Flippa', 'Martha', 'O', 'St. Elizabeth st.', 'Cebu City', '9821', '988721241', 'Flippa@example.com'),
+(16, 'George', 'Found', 'H', 'St. Francis st.', 'Naga City', '21313', '988212311', 'George@example.com'),
+(17, 'Santa', 'Clause', 'G', 'St. Nicolas street', 'Artic', '1212', '3123123123', 'santa@example.com'),
+(18, 'Marzi', 'Filma', 'G', 'st. nicolas street', 'Cebu', '12313', '123123', '@example.com');
 
 -- --------------------------------------------------------
 
@@ -204,15 +230,27 @@ CREATE TABLE IF NOT EXISTS `plot_ownership` (
   KEY `grave_id` (`grave_id`),
   KEY `owner_id` (`owner_id`),
   KEY `cemetery_id` (`cemetery_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `plot_ownership`
 --
 
 INSERT INTO `plot_ownership` (`plot_id`, `grave_id`, `owner_id`, `cemetery_id`, `date_purchased`, `purchase_price`, `ownership_status`, `square_meters`) VALUES
-(2, NULL, 1, 3, '2022-10-12', 90211, 'active', '2.5 x 8 feet'),
-(3, NULL, 2, 3, '2022-10-15', 87112, 'active', '3 x 8 meters');
+(27, 1, 12, 3, '2022-11-01', 111, 'active', 'sqr1'),
+(28, 2, 12, 3, '2022-11-01', 11111, 'active', 'sqr11'),
+(29, NULL, 13, 3, '2022-11-02', 222, 'active', 'sqr2'),
+(30, NULL, 13, 3, '2022-11-02', 22222, 'active', 'sqr22'),
+(31, NULL, 14, 3, '2022-11-03', 333, 'active', 'sqr3'),
+(32, NULL, 14, 3, '2022-11-03', 33333, 'active', 'sqr33'),
+(33, NULL, 15, 3, '2022-11-04', 444, 'active', 'sqr4'),
+(34, NULL, 15, 3, '2022-11-04', 44444, 'active', 'sqr44'),
+(35, NULL, 16, 3, '2022-11-05', 555, 'active', 'sqr5'),
+(36, NULL, 12, 3, '2022-11-19', 123, 'active', '2.6 x 9 meters'),
+(37, NULL, 17, 2, '2022-12-30', 6969, 'active', '2.6 x 9 meters'),
+(38, NULL, 17, 2, '2022-11-12', 1212, 'active', '2.6 x 9 meters'),
+(39, NULL, 18, 2, '2022-11-19', 12312312, 'active', '2.6 x 9 meters'),
+(40, NULL, 18, 2, '2022-11-18', 11111, 'active', '2.5 x 8 meters');
 
 --
 -- Constraints for dumped tables
